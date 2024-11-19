d313c2fca7c0b34e9b4682fc9de9ddba03a2a123
WeiJun Liu <30994302+LiuWeijunAntihos@users.noreply.github.com>
2024-11-19 05:37:19 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,57 +4,61 @@ set -e
 
 echo "====================="
 echo "0. Prepare for launch dotfile."
-# in docker
-if [ `whoami` == 'root' ];then
-    apt-get update && apt-get install -y sudo
+
+# Determine the current user
+if [ "$(whoami)" = "root" ]; then
+    SUDO=""
+else
+    SUDO="sudo"
 fi
 
-sudo apt update && sudo apt-get install -y python3-distutils curl wget
-# get pip3
-curl -O https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py
+# Update and install necessary packages
+$SUDO apt-get update && $SUDO apt-get install -y python3-distutils curl wget
 
-# install zsh
-sudo apt-get update && sudo apt-get install -f -y zsh autojump git
-chsh -s /bin/zsh
+# Get pip3
+curl -O https://bootstrap.pypa.io/get-pip.py && $SUDO python3 get-pip.py
+
+# Install zsh
+$SUDO apt-get update && $SUDO apt-get install -y zsh autojump git
+$SUDO chsh -s /bin/zsh
 
 echo "====================="
 echo "1. Backup your config files. e.g. ~/.zshrc -> ~/.zshrc.backup"
-# Require Vim with Lua
-if [ -f "$HOME/.zshrc" ];then
-  echo "$file found."
-  mv ~/.zshrc ~/.zshrc.backup
+
+# Backup existing .zshrc file
+if [ -f "$HOME/.zshrc" ]; then
+    echo "$HOME/.zshrc found."
+    mv "$HOME/.zshrc" "$HOME/.zshrc.backup"
 else
-  echo "$file not found."
+    echo "$HOME/.zshrc not found."
 fi
 
 echo "====================="
-echo "2. Install zsh plugin"
+echo "2. Install zsh plugins"
 
-# install oh-my-zsh
-rm -rf ~/.oh-my-zsh
+# Install oh-my-zsh
+rm -rf "$HOME/.oh-my-zsh"
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
-sudo cp zshrc ~/.zshrc
-sudo cp my-shortkey ~/.oh-my-zsh/
+$SUDO cp zshrc "$HOME/.zshrc"
+$SUDO cp my-shortkey "$HOME/.oh-my-zsh/"
 
 echo "====================="
-echo "3. Install Lazy vim"
+echo "3. Install LazyVim"
 
-sudo apt-get install -f -y software-properties-common build-essential
+$SUDO apt-get install -y software-properties-common build-essential
 
-# install neo vim
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update && sudo apt-get install -f -y neovim lua5.3
+# Install neovim
+$SUDO add-apt-repository ppa:neovim-ppa/unstable
+$SUDO apt-get update && $SUDO apt-get install -y neovim lua5.3
 
-# install LazyVim
-rm -rf ~/.config/nvim ~/.local/share/nvim 
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
-
+# Install LazyVim
+rm -rf "$HOME/.config/nvim" "$HOME/.local/share/nvim"
+git clone https://github.com/LazyVim/starter "$HOME/.config/nvim"
+rm -rf "$HOME/.config/nvim/.git"
 
 echo "====================="
 echo "Success!"
-
